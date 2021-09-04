dc05524354e796d4f60732c1d86221d7a960c1c3
yangrtc <yangrtc@aliyun.com>
2021-09-04 19:44:33 +0800

    2.0.001

--- YangRecord/src/yangwinutil/yangrecordcontext.cpp
+++
@@ -1,8 +0,0 @@
-#include "yangrecordcontext.h"
-#include "recordmainwindow.h"
-YangRecordVideoContext::YangRecordVideoContext()
-{
-
-}
-YangRecordThread *YangRecordVideoContext::yangThread=nullptr;
-RecordMainWindow *YangRecordVideoContext::mainWin=nullptr;
