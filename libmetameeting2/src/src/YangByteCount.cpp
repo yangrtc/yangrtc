5a0669f9e83ec86a8eb3dea1b20234c9fbdf6f4c
yangrtc <yangrtc@aliyun.com>
2022-02-06 11:01:37 +0800

    2.0.120

---
+++ libmetameeting2/src/src/YangByteCount.cpp
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+#include "YangByteCount.h"
+
+YangByteCount::YangByteCount() {
+	// TODO Auto-generated constructor stub
+
+}
+
+YangByteCount::~YangByteCount() {
+	// TODO Auto-generated destructor stub
+}
+
