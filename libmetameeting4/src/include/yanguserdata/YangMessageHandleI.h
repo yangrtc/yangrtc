d284187094ec224b8ebe7e94c78ee6f2d9c5c53c
yangrtc <yangrtc@aliyun.com>
2022-06-12 20:06:03 +0800

    4.0.050

---
+++ libmetameeting4/src/include/yanguserdata/YangMessageHandleI.h
@@ -0,0 +1,17 @@
+﻿//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+#ifndef SRC_YANGDATACHANNEL_INCLUDE_YANGMESSAGETCPI_H_
+#define SRC_YANGDATACHANNEL_INCLUDE_YANGMESSAGETCPI_H_
+#include <stdint.h>
+
+class YangMessageHandleI{
+public:
+	YangMessageHandleI(){}
+	virtual ~YangMessageHandleI(){}
+	virtual void decodeMessage(uint8_t* p,int32_t plen)=0;
+};
+
+
+
+#endif /* SRC_YANGDATACHANNEL_INCLUDE_YANGMESSAGETCPI_H_ */
