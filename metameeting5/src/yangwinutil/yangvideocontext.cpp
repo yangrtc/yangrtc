0638db4ff6432e40e530f670339939d73eb5f3d2
yangrtc <yangrtc@aliyun.com>
2022-06-30 21:34:35 +0800

    5.0-b0

---
+++ metameeting5/src/yangwinutil/yangvideocontext.cpp
@@ -0,0 +1,24 @@
+﻿//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+#include "yangvideocontext.h"
+
+YangVideoContext::YangVideoContext()
+{
+    videoMeeting=NULL;
+     videoThread=NULL;
+    context=NULL;
+    userinfo=NULL;
+    meeting=NULL;
+    mainWin=NULL;
+}
+YangVideoContext::~YangVideoContext()
+{
+    videoMeeting=NULL;
+     videoThread=NULL;
+    context=NULL;
+    userinfo=NULL;
+    meeting=NULL;
+    mainWin=NULL;
+}
+
